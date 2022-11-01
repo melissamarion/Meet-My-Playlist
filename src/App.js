b756ae29e2eb1da62eed76545dbae98eb5a254b2
Melissa Perry <melissamarionperry@gmail.com>
2022-11-01 12:32:30 -0400

    Basic fondation set

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ const App = () => {
   return (
     <div>
       <h1>Meet My Playlist</h1>
+      <h2>User 1</h2>
+      <input type="text" />
+      <h2>User 2</h2>
+      <input type="text" />
+      <h3>Will I see you at the show!?</h3>
+      <h3>YES</h3>
+      <h3>NO</h3>
     </div>
   )
 }
