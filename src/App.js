e153c1304e39ee3db612cc81a54bacd106652158
Melissa Perry <melissamarionperry@gmail.com>
2022-11-02 23:03:58 -0400

    need to shape componentsgit add .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css'
 import PlayList from './Components/PlayList.js'
+import SideBar from './Components/SideBar.js'
+import Main from './Components/Main.js'
 
 const App = () => {
   return (
     <div>
+      <h1>Meet My Playlist</h1>
       <PlayList text="Genre:" />
       <PlayList text="Title:" />
       <PlayList text="Count:" />
       <PlayList text="Song:" />
+      <br></br> <br></br> <br></br>
+      <PlayList text="Genre:" />
+      <PlayList text="Title:" />
+      <PlayList text="Count:" />
+      <PlayList text="Song:" />
+      <br></br> <br></br> <br></br>
+      <PlayList text="Genre:" />
+      <PlayList text="Title:" />
+      <PlayList text="Count:" />
+      <PlayList text="Song:" />
+      <br></br> <br></br> <br></br>
+      <PlayList text="Genre:" />
+      <PlayList text="Title:" />
+      <PlayList text="Count:" />
+      <PlayList text="Song:" />
+      <SideBar />
+      <Main />
     </div>
   )
 }
--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,2 +1,9 @@
+const wordsArray = [
+  "Human Fly, Psychobilly Freakout, Miss Take, Outlaw Heart, Ride The Torpedo, Who Killed The Cheerleader, The Bottle Called, Wreckin Crew, Bodies In The Basement, King Rat, My Curse, Werewolves, Werewolf Boogie, Gravedigger Rock, Walk Like A Zombie, Way I Walk, Zombies Ate Her Brain, Freaks Come Out, 9 Lives, The Bug Of Planet Zee, Dancing With The Devil, In The Orchard, Horny In A Hearse, Rock N Roll Zombie, Death Row, Psychobilly, Psycho For Your Love, The Dead Don't Get Older, Rockin Coffin Bar, Bloodstains, Track 07, 1000 Kats, C.O.CK.R.O.A.C.H.E.S, My Girl, Transsylvanian Express, I Was A Teenage Werewolf, Its Martini Time, Thelma And Louise, Psychobilly Stomp, Take A Razor To Your Head, Graveyard Tree, California Hell, Kicking Satan Out Of Hell, Skeleton Swamp, Dead Moon, Bikini Girls With Machine Guns, Kiss Me Now, Like The Wolf That Howls"
+]
 
+const result = wordsArray.map((word) => {
+  return word + '!'
+})
 
+console.log(result)
