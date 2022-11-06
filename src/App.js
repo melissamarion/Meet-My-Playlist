256d881881f6293a98634b6a74029b499f0d2810
Melissa Perry <melissamarionperry@gmail.com>
2022-11-06 16:47:26 -0500

    solved json problems to import without breaking code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import music from './data.json'
 import PlayList from './Components/PlayList.js'
 import SideBar from './Components/SideBar.js'
 import Main from './Components/Main.js'
 import Footer from './Components/Footer.js'
 
 const App = () => {
+  // function App() {
   return (
     <div className="flex-row">
       <PlayList />
